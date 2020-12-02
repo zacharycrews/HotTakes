59e259928ecf8ebf77917aaf1881f98b711a5b75
Zach Crews <crewsz@bc.edu>
2020-12-02 16:41:59 -0500

    Initial Commit

---
+++ HotTakes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotTakes
+//
+//  Created by Zach Crews on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
